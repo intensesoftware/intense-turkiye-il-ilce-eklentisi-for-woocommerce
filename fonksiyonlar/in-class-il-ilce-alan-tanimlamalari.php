083408ab017013a79fc295c18633237efafb2938
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2019-12-05 05:02:42 +0300

    Ödeme sayfası alan sıralaması 'ad -> soyad -> firma -> ülke -> şehir -> ilçe -> adres_1 -> adres_2 -> pk' olarak düzenlendi.

--- fonksiyonlar/in-class-il-ilce-alan-tanimlamalari.php
+++ fonksiyonlar/in-class-il-ilce-alan-tanimlamalari.php
@@ -13,9 +13,66 @@ Class In_Class_Il_Ilce_Alan_Tanimlamalari {
 
         add_filter('woocommerce_checkout_fields', array( $this, 'ilce_alaninin_tanimlanmasi_ve_oncelik_siralamalari' ), 9999);
 
-        add_action('woocommerce_default_address_fields', array( $this, 'adres_alanlari_oncelik_siralamalari' ), 9999);
+        add_filter('woocommerce_checkout_fields', array( $this, 'odeme_alanlarinin_son_siralama_kontrolu' ), 10000);
+
+        add_filter('woocommerce_default_address_fields', array( $this, 'adres_alanlari_oncelik_siralamalari' ), 9999);
 
         add_action('woocommerce_before_checkout_billing_form', array( $this, 'secili_ilce_bilgileri' ) );
+
+    }
+
+
+    function odeme_alanlarinin_son_siralama_kontrolu( $fields ){
+
+        $woocommerce_default_alanlar = array(
+            'billing_state',
+            'billing_first_name',
+            'billing_last_name',
+            'billing_company',
+            'billing_country',
+            'billing_address_1',
+            'billing_address_2',
+            'billing_city',
+            'billing_postcode',
+            'billing_phone',
+            'billing_email',
+            'shipping_first_name',
+            'shipping_last_name',
+            'shipping_company',
+            'shipping_country',
+            'shipping_address_1',
+            'shipping_city',
+            'shipping_state',
+            'shipping_address_2',
+            'shipping_postcode',
+            'billing_ilce',
+            'shipping_ilce'
+        );
+
+
+        foreach( $fields as $alanlar_key => $alanlar ){
+
+            if( $alanlar_key != 'billing' && $alanlar_key != 'shipping' )
+                continue;
+
+
+            foreach( $alanlar as $alan_key => $alan){
+
+
+                if(!in_array($alan_key, $woocommerce_default_alanlar)){
+
+                    $fields[$alanlar_key][$alan_key]['priority'] += 30;
+
+                }
+
+
+            }
+
+
+        }
+
+        return $fields;
+
 
     }
 
@@ -37,11 +94,15 @@ Class In_Class_Il_Ilce_Alan_Tanimlamalari {
 
 
 
-        $country_priority = $address_fields['country']['priority'];
-        $address_fields['state']['priority'] = $country_priority + 1;
-        $address_fields['city']['priority'] = $country_priority + 2;
-        $address_fields['address_1']['priority'] = $country_priority + 3;
-        $address_fields['address_2']['priority'] = $country_priority + 4;
+        $address_fields['first_name']['priority'] = 1;
+        $address_fields['last_name']['priority'] = 2;
+        $address_fields['company']['priority'] = 3;
+        $address_fields['country']['priority'] = 4;
+        $address_fields['state']['priority'] = 5;
+        $address_fields['city']['priority'] = 6;
+        $address_fields['address_1']['priority'] = 7;
+        $address_fields['address_2']['priority'] = 8;
+        $address_fields['postcode']['priority'] = 9;
 
 
         return $address_fields;
